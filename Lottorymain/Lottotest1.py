1395ac7b40d0522f6d0c6017bfbaecf0759e340f
Keebinlam <140448996+Keebinlam@users.noreply.github.com>
2023-10-27 15:11:23 -0600

    cleanup

---
+++ Lottorymain/Lottotest1.py
@@ -0,0 +1,7 @@
+# the goal for this is to break down all the lottery numbers into separare columns, find the most frequent numbers in each column, show how many of each number appear on a chart
+import pandas
+import math
+
+df = pandas.read_csv(
+    "/Users//Desktop/Python project/Projects/files/Lottery_Powerball_Winning_Numbers__Beginning_2010.csv")
+print(df)

80825ba798a4dfa64c9df24795c8386764fb7e58
Keebinlam <140448996+Keebinlam@users.noreply.github.com>
2023-10-28 10:33:32 -0600

    extranotes

--- Lottorymain/Lottotest1.py
+++ Lottorymain/Lottotest1.py
@@ -1,3 +1,4 @@
+# add the highist nunbers combination and get the range of where most number lands on
 # the goal for this is to break down all the lottery numbers into separare columns, find the most frequent numbers in each column, show how many of each number appear on a chart
 import pandas
 import math
